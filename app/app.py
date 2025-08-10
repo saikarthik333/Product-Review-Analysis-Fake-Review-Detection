ecc8948d81c8efab649c555445e06883a48a2ee7
Motapothula Sai Karthik <saikarthikmotapothula333@gmail.com>
2025-08-10 18:45:28 +0530

    Fix model performance with balanced data and improved features

--- app/app.py
+++ app/app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:26956b0f5547ae0ffa7741093e2f6acc01e5d8d88a7da6f8c0ae53e231c5a483
-size 4454
+oid sha256:7aaf6c392e7823d6d8bcaec616551937635c76b53a2162f1b02fe81ac8b4f603
+size 4907
